4675a569321017451a91ee7f3ebe9f6f26aa4464
mexiricas <mexiricas@hotmail.com>
2022-06-03 21:27:28 -0300

    Inserindo listAll para adocoes

--- src/main/java/br/com/treinamento/adoteUmPet/api/controler/AdocaoControler.java
+++ src/main/java/br/com/treinamento/adoteUmPet/api/controler/AdocaoControler.java
@@ -7,9 +7,13 @@ import br.com.treinamento.adoteUmPet.repositories.AdocaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class AdocaoControler {
@@ -18,7 +22,15 @@ public class AdocaoControler {
     @Autowired
     private AdocaoMapper adocaoMapper;
 
-
+    @GetMapping("/adocoes")
+    public List<AdocaoResponse> listAll (){
+        var adocoes =  adocaoRepository.findAll();
+        List<AdocaoResponse> adocaoLista = adocoes
+                .stream()
+                .map(p -> adocaoMapper.toAResponse(p))
+                .collect(Collectors.toList());
+        return adocaoLista;
+    }
     @PostMapping("/adocoes")
     public ResponseEntity<AdocaoResponse>  create(@RequestBody AdocaoResquest adocaoResquest){
         var adocao = adocaoMapper.toAdocaoModel(adocaoResquest);
